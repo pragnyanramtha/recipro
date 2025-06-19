6a72de1735d20b2eca69b1778121e3bd27abbb3c
pragnyanramtha <pragnyanram@gmail.com>
2025-06-19 14:02:11 +0000

    suppressed warnings 2

--- src/recipro_backend/src/lib.rs
+++ src/recipro_backend/src/lib.rs
@@ -1,15 +1,20 @@
-use ic_cdk_macros::*;
 // use sudograph::graphql_database;
+// sudograph not yet supportted
+
 
 // This macro generates all the necessary Rust code for your GraphQL database.
 // It reads your schema.graphql file and creates the types, queries, and mutations.
 // graphql_database!("src/my_backend_canister/schema.graphql");
+
+//ADD database after supported version added
 
 // You can add other standard canister functions if needed,
 // but Sudograph handles the core GraphQL API.
 
 // Example: A simple canister info function (not part of Sudograph's auto-gen)
 
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+//fn greet(name: String) -> String {
+//    format!("Hello, {}!", name)
+//}
+
+// Pointless
